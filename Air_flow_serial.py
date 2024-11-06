a5751a6be88a2c5095fba4cabdfd19ffba7c52cd
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-11-06 23:46:29 +0900

    Add files via upload

---
+++ Air_flow_serial.py
@@ -0,0 +1,10 @@
+# Air_flow_serial.py
+
+# DAQ_serial 모듈에서 DaqSerial 클래스와 호흡 데이터 요청 프로토콜 ID 상수 가져오기
+from DAQ_Serial import DaqSerial, RASPI_PROTOCOL_TO_DAQ_CMD_ID_REQ_AIR_FLOW
+from daq_handler import setup_daq_serial, read_data
+
+# DaqSerial 인스턴스 생성 및 시리얼 통신 열기
+daq_serial = setup_daq_serial(RASPI_PROTOCOL_TO_DAQ_CMD_ID_REQ_AIR_FLOW)
+# 실제로 데이터를 읽어오는 로직
+read_data(daq_serial)
